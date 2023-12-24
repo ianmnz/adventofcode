574daf98d1d60bd11f9dda3af1aa6ea0cd0864fd
ianmnz <ianmemar@gmail.com>
2023-12-24 12:08:28 +0100

    AoC 2023 22 - Improve performance

--- 2023/22/main.py
+++ 2023/22/main.py
@@ -2,8 +2,8 @@
 # https://adventofcode.com/2023/day/22
 
 import collections
-from typing import List, Iterable, Tuple
-from dataclasses import dataclass
+from typing import List, Iterable, Tuple, Set, Dict
+from dataclasses import dataclass, field
 
 
 Position = collections.namedtuple('Position', ['x', 'y', 'z'])
@@ -12,6 +12,9 @@ Position = collections.namedtuple('Position', ['x', 'y', 'z'])
 class Brick:
     left: Position
     right: Position
+
+    stack_up: Set['Brick'] = field(init=False, default_factory=lambda: set(), repr=False)
+    stack_on: Set['Brick'] = field(init=False, default_factory=lambda: set(), repr=False)
 
     def base(self) -> int:
         return min(self.left.z, self.right.z)
@@ -28,39 +31,66 @@ class Brick:
         return Brick(Position(self.left.x, self.left.y, self.left.z - height),
                      Position(self.right.x, self.right.y, self.right.z - height))
 
+    def stackpush(self, above: 'Brick') -> None:
+        self.stack_up.add(above)
+        above.stack_on.add(self)
+
     def __lt__(self, other: 'Brick') -> bool:
         return self.base() < other.base()
 
+    def __hash__(self) -> int:
+        return hash((*self.left, *self.right))
 
-def simulate(bricks: List[Brick]) -> Tuple[int, List[Brick]]:
-    top_down_view = collections.defaultdict(int)
+    def __eq__(self, other: 'Brick') -> bool:
+        return self.left == other.left and self.right == other.right
+
+
+def simulate(bricks: List[Brick]) -> List[Brick]:
+    top_down_view = dict()
     settled = []
-    nb_falls = 0
 
     for brick in bricks:
-        peak = max(top_down_view[(x, y)] for x, y in brick.shadow())
+        peak = max(top_down_view[(x, y)].top() if (x,y) in top_down_view else 0 for x, y in brick.shadow())
         height = max(0, brick.base() - peak - 1)
 
         if height > 0:
-            nb_falls += 1
             brick = brick.drop(height)
 
         settled.append(brick)
 
         for x, y in brick.shadow():
-            top_down_view[(x, y)] = brick.top()
+            if (x, y) in top_down_view and top_down_view[(x, y)].top() == brick.base() - 1:
+                top_down_view[(x, y)].stackpush(brick)
+            top_down_view[(x, y)] = brick
 
-    return nb_falls, settled
+    return settled
+
+
+def count_falls_if_removed(brick: Brick, support: Dict[Brick, int]) -> int:
+    fallen = [brick]
+    nb_falls = 0
+    while fallen:
+        base = fallen.pop()
+
+        for stacked_up in base.stack_up:
+            support[stacked_up] -= 1
+
+            if support[stacked_up] == 0:
+                fallen.append(stacked_up)
+                nb_falls += 1
+
+    return nb_falls
 
 
 def bricks_to_disintegrate(data: List[Brick]) -> Tuple[int]:
-    _, settled = simulate(sorted(data))
+    settled = simulate(sorted(data))
 
     safe = 0
     count = 0
-    for idx in range(len(settled)):
-        stack = settled[:idx] + settled[idx + 1:]
-        nb_falls, _ = simulate(stack)
+    support = {brick: len(brick.stack_on) for brick in settled}
+
+    for brick in settled:
+        nb_falls = count_falls_if_removed(brick, support.copy())
         safe += int(nb_falls == 0)
         count += nb_falls
 
@@ -89,7 +119,6 @@ def main():
 
     # --- Part 1 --- #
     with Timer():
-        # print(solve(snapshot))
         print("Nb of brick to disintegrate:", bricks_to_disintegrate(data)[0])  # 468
 
     # --- Part 2 --- #
