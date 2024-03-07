1ef5995bdefb64c2076d1f9c70ea17beaf4ef9ad
ianmnz <ianmemar@gmail.com>
2024-03-07 20:51:14 +0100

    AoC 2022 03

--- 2022/03/main.py
+++ 2022/03/main.py
@@ -1,74 +1,99 @@
 # Advent of Code : Day 03 - Rucksack Reorganization
 # https://adventofcode.com/2022/day/3
 
+from itertools import islice
+from typing import Generator, Iterable, List, Tuple
+
+from helpers import Timer
+
 
 def priority(item: str) -> int:
     prio = {
-        'a' :  1,
-        'b' :  2,
-        'c' :  3,
-        'd' :  4,
-        'e' :  5,
-        'f' :  6,
-        'g' :  7,
-        'h' :  8,
-        'i' :  9,
-        'j' : 10,
-        'k' : 11,
-        'l' : 12,
-        'm' : 13,
-        'n' : 14,
-        'o' : 15,
-        'p' : 16,
-        'q' : 17,
-        'r' : 18,
-        's' : 19,
-        't' : 20,
-        'u' : 21,
-        'v' : 22,
-        'w' : 23,
-        'x' : 24,
-        'y' : 25,
-        'z' : 26
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26,
     }
     if item.isupper():
         return 26 + prio[item.lower()]
     else:
         return prio[item]
 
-def main() -> None:
+
+@Timer.timeit
+def find_priority_of_common(rucksacks: List[str]) -> int:
     sum_common_type_prio = 0
+
+    for rucksack in rucksacks:
+        middle = len(rucksack) // 2
+        common = set(rucksack[:middle]) & set(rucksack[middle:])
+        sum_common_type_prio += priority(list(common)[0])
+
+    return sum_common_type_prio
+
+
+@Timer.timeit
+def find_priority_of_badges(rucksacks: List[str]) -> int:
+    def batched(iterable: Iterable, chunk_size: int) -> Generator[Tuple, None, None]:
+        iterator = iter(iterable)
+        while chunk := tuple(islice(iterator, chunk_size)):
+            yield chunk
+
     sum_badge_type_prio = 0
 
-    with open('input.txt', 'r') as file:
-        for count, line in enumerate(file):
-            line = line.strip()
-            compartment_size = len(line)//2
+    for first, second, third in batched(rucksacks, 3):
+        badge = set(first) & set(second) & set(third)
+        sum_badge_type_prio += priority(list(badge)[0])
 
-            # --- Part 1 ---
-            first_compartment = set(line[:compartment_size])
-            second_compartment = set(line[compartment_size:])
+    return sum_badge_type_prio
 
-            common_type = list(first_compartment.intersection(second_compartment))[0]
-            sum_common_type_prio += priority(common_type)
 
-            # --- Part 2 ---
-            elf_badge = set(line)
+@Timer.timeit
+def parse(filename: str) -> List[str]:
+    with open(filename, "r") as file:
+        rucksacks = file.read().strip().split()
+    return rucksacks
 
-            if count % 3 == 0:
-                group_badge = elf_badge
-            else:
-                group_badge = group_badge.intersection(elf_badge)
 
-                if count % 3 == 2:
-                    badge_type = list(group_badge)[0]
-                    sum_badge_type_prio += priority(badge_type)
+@Timer.timeit
+def solve(filename: str) -> Tuple[int, int]:
+    rucksacks = parse(filename)
+    part1 = find_priority_of_common(rucksacks)
+    part2 = find_priority_of_badges(rucksacks)
 
-    # Answer part 1 :
-    print(f"Sum of common type priorities: {sum_common_type_prio}") # 8515
+    return part1, part2
 
-    # Answer part 2 :
-    print(f"Sum of badge type priorities: {sum_badge_type_prio}") # 2434
+
+def main() -> None:
+    import os
+
+    res = solve(os.path.dirname(os.path.abspath(__file__)) + "/input.txt")
+
+    assert res[0] == 8515, f"Part1 = {res[0]}"
+    assert res[1] == 2434, f"Part2 = {res[1]}"
+
 
 if __name__ == "__main__":
     main()
