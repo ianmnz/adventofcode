aeef8bb3a23f2df6bcd1ade7731e8ceb473cd96d
ianmnz <ianmemar@gmail.com>
2024-03-08 20:57:44 +0100

    AoC 2022 07

--- 2022/07/main.py
+++ 2022/07/main.py
@@ -2,144 +2,147 @@
 # https://adventofcode.com/2022/day/7
 
 
-from typing import Dict, Optional
+from dataclasses import dataclass, field
+from functools import cached_property
+from typing import Dict, List, Optional, Tuple
+
+from helpers import Timer
 
 
+@dataclass
 class File:
-    def __init__(self, name: str, size: int) -> None:
-        self._name: str = name
-        self._size: int = size
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def size(self) -> int:
-        return self._size
+    name: str
+    du: int
 
 
+@dataclass
 class Dir:
-    def __init__(self, name: str, parent: Optional['Dir'] = None) -> None:
-        self._name: str = name
-        self._parent = parent
-        self._childs: Dict[str, Dir] = dict()
-        self._files: Dict[str, File] = dict()
+    name: str
+    parent: Optional["Dir"] = field(default=None)
+    children: Dict[str, "Dir"] = field(init=False, default_factory=dict)
+    files: List[File] = field(init=False, default_factory=list)
 
-    @property
-    def name(self) -> str:
-        return self._name
+    def cd(self, child_name: str) -> "Dir":
+        return self.children[child_name]
 
-    @property
-    def parent(self) -> Optional['Dir']:
-        return self._parent
-
-    @property
-    def childs(self) -> Dict[str, 'Dir']:
-        return self._childs
-
-    def has_child(self, child_name: str) -> bool:
-        return child_name in self._childs
-
-    def child(self, child_name: str) -> 'Dir':
-        return self._childs[child_name]
-
-    def add_child(self, child_name: str) -> 'Dir':
+    def mkdir(self, child_name: str) -> "Dir":
         child = Dir(child_name, self)
-        self._childs[child_name] = child
+        self.children[child_name] = child
         return child
 
-    @property
-    def files(self) -> Dict[str, File]:
-        return self._files
-
-    def add_file(self, file_name: str, file_size: int) -> File:
+    def touch(self, file_name: str, file_size: int) -> File:
         file = File(file_name, file_size)
-        self._files[file_name] = file
+        self.files.append(file)
         return file
 
-    @property
-    def size(self) -> int:
-        size = 0
-        for file in self._files.values():
-            size += file.size
-
-        for dir in self._childs.values():
-            size += dir.size
-
-        return size
+    @cached_property
+    def du(self) -> int:
+        # Recursively computes the size of the directory
+        return sum(file.du for file in self.files) + sum(
+            child.du for child in self.children.values()
+        )
 
 
-def main():
-    n = 100000
+@Timer.timeit
+def build_filesystem(terminal_output: List[str]) -> Dir:
+    outermost_dir = Dir("/")
+    current_dir = outermost_dir
+
+    for line in terminal_output:
+        parsed_line = line.split()
+
+        if parsed_line[0] == "$":  # Command
+            if parsed_line[1] == "cd":  # Cd
+                if parsed_line[2] == "/":
+                    current_dir = outermost_dir
+
+                elif parsed_line[2] == "..":
+                    current_dir = current_dir.parent
+
+                else:
+                    current_dir = current_dir.cd(parsed_line[2])
+
+            elif parsed_line[1] == "ls":
+                continue
+
+        else:  # Listing
+            if parsed_line[0] == "dir":
+                current_dir.mkdir(parsed_line[1])
+
+            elif parsed_line[0].isnumeric():
+                current_dir.touch(parsed_line[1], int(parsed_line[0]))
+
+    return outermost_dir
+
+
+@Timer.timeit
+def sum_dir_sizes_leq_threshold(root: Dir, size_max: int = 100_000) -> int:
     sum_dir_total_size_leq_n = 0
 
-    total_disk_space = 70000000
-    needed_space = 30000000
+    dirs = [root]
+    while dirs:
+        curr = dirs.pop()
 
-    outermost_dir = Dir('/')
-    dirs = [outermost_dir]
-    dirs_sizes = dict()
+        if curr.du <= size_max:
+            sum_dir_total_size_leq_n += curr.du
 
-    with open('input.txt', 'r') as file:
-        for line in file:
-            line = line.split()
+        for child in curr.children.values():
+            dirs.append(child)
 
-            if line[0] == '$': # Command
-                if line[1] == 'cd': # Cd
-                    if line[2] == '/':
-                        current_dir = outermost_dir
-
-                    elif line[2] == '..':
-                        current_dir = current_dir.parent
-                    else:
-                        if current_dir.has_child(line[2]):
-                            current_dir = current_dir.child(line[2])
-                        else:
-                            print(f"Error : {current_dir.name} does not have child {line[2]}")
-
-                elif line[1] == 'ls':
-                    continue
-
-            else: # Listing
-                if line[0] == 'dir':
-                    child = current_dir.add_child(line[1])
-                    dirs.append(child)
-
-                elif line[0].isnumeric():
-                    current_dir.add_file(line[1], int(line[0]))
-
-    # --- Part 1 --- #
-    for dir in dirs:
-        dir_size = dir.size
-        dirs_sizes[dir.name] = dir_size
-        if dir_size < n:
-            sum_dir_total_size_leq_n += dir_size
-
-    # Answer part 1 :
-    print(f'Sum of total sizes of at most {n}: {sum_dir_total_size_leq_n}') # 1454188
+    return sum_dir_total_size_leq_n
 
 
-    # --- Part 2 --- #
-    total_used_space = dirs_sizes[outermost_dir.name]
+@Timer.timeit
+def find_size_smallest_dir_to_be_deleted(
+    root: Dir, total_disk_space: int = 70_000_000, needed_space: int = 30_000_000
+) -> int:
+    total_used_space = root.du
     total_unused_space = total_disk_space - total_used_space
     space_to_be_freed = needed_space - total_unused_space
 
-    if space_to_be_freed > 0:
-        print(f'Needed space: {space_to_be_freed}')
+    if space_to_be_freed <= 0:
+        return 0
 
-        smallest_dir_to_be_deleted_name = outermost_dir.name
-        smallest_dir_to_be_deleted_size = total_used_space
+    smallest_dir_to_be_deleted_size = total_used_space
 
-        for dir_name, dir_size in dirs_sizes.items():
-            if dir_size > space_to_be_freed:
-                if dir_size < smallest_dir_to_be_deleted_size:
-                    smallest_dir_to_be_deleted_name = dir_name
-                    smallest_dir_to_be_deleted_size = dir_size
+    dirs = [root]
+    while dirs:
+        curr = dirs.pop()
 
-        # Answer part 2 :
-        print(f'Directory to be deleted: {smallest_dir_to_be_deleted_name}') # wvq
-        print(f'Total size of directory to be deleted: {smallest_dir_to_be_deleted_size}') # 4183246
+        if space_to_be_freed <= curr.du < smallest_dir_to_be_deleted_size:
+            smallest_dir_to_be_deleted_size = curr.du
+
+        for child in curr.children.values():
+            dirs.append(child)
+
+    return smallest_dir_to_be_deleted_size
+
+
+@Timer.timeit
+def parse(filename: str) -> List[str]:
+    with open(filename, "r") as file:
+        terminal_output = file.read().strip().split("\n")
+    return terminal_output
+
+
+@Timer.timeit
+def solve(filename: str) -> Tuple[int, int]:
+    terminal_output = parse(filename)
+    root = build_filesystem(terminal_output)
+    part1 = sum_dir_sizes_leq_threshold(root)
+    part2 = find_size_smallest_dir_to_be_deleted(root)
+
+    return part1, part2
+
+
+def main() -> None:
+    import os
+
+    res = solve(os.path.dirname(os.path.abspath(__file__)) + "/input.txt")
+
+    assert res[0] == 1454188, f"Part1 = {res[0]}"
+    assert res[1] == 4183246, f"Part2 = {res[1]}"
+
 
 if __name__ == "__main__":
     main()
