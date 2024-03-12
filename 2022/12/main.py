c9f69b593d6655abde3721d8531af1472d5afa05
ianmnz <ianmemar@gmail.com>
2024-03-12 11:27:50 +0100

    AoC 2022 12 - Add other shortest path methods

--- 2022/12/main.py
+++ 2022/12/main.py
@@ -2,7 +2,8 @@
 # https://adventofcode.com/2022/day/12
 
 
-from collections import deque
+import heapq
+from collections import deque, namedtuple
 from dataclasses import dataclass, field
 from typing import Dict, List, Tuple
 
@@ -49,50 +50,62 @@ class Node:
     val: int
     adj: List[complex] = field(init=False, default_factory=list)
 
+    def __lt__(self, other: "Node") -> bool:
+        return (self.pos.real, self.pos.imag) < (other.pos.real, other.pos.imag)
+
+
+ASearch = namedtuple("ASearch", ["f", "g", "h"])
+
 
 @Timer.timeit
 def build_graph(
     heightmap: List[str],
-) -> Tuple[Dict[complex, Node], complex, complex, List[complex]]:
+) -> Tuple[Dict[complex, Node], complex, List[complex]]:
     m, n = len(heightmap), len(heightmap[0])
 
     is_valid = lambda x, y: (0 <= x < m) and (0 <= y < n)
     height = lambda x, y: char_to_int(heightmap[x][y])
 
-    start = 0j
     target = 0j
     candidates = []
     nodes = {}
     for i, row in enumerate(heightmap):
         for j, col in enumerate(row):
             node = Node(complex(i, j), char_to_int(col))
-
-            if col == "S":
-                start = node.pos
-            elif col == "E":
-                target = node.pos
-            elif col == "a":
-                candidates.append(node.pos)
+            adj = []
 
             if is_valid(i - 1, j) and height(i - 1, j) <= node.val + 1:
                 node.adj.append(complex(i - 1, j))  # Up
+                adj.append(heightmap[i - 1][j])
 
             if is_valid(i + 1, j) and height(i + 1, j) <= node.val + 1:
                 node.adj.append(complex(i + 1, j))  # Down
+                adj.append(heightmap[i + 1][j])
 
             if is_valid(i, j - 1) and height(i, j - 1) <= node.val + 1:
                 node.adj.append(complex(i, j - 1))  # Left
+                adj.append(heightmap[i][j - 1])
 
             if is_valid(i, j + 1) and height(i, j + 1) <= node.val + 1:
                 node.adj.append(complex(i, j + 1))  # Right
+                adj.append(heightmap[i][j + 1])
+
+            if col == "S":
+                candidates = [node.pos] + candidates
+            elif col == "E":
+                target = node.pos
+            elif (col == "a") and (
+                "b" in adj
+            ):  # We consider only 'a's that can eventually go uphill
+                candidates.append(node.pos)
 
             nodes[node.pos] = node
 
-    return nodes, target, start, candidates
+    return nodes, target, candidates
 
 
 def bfs(graph: Dict[complex, Node], start: complex, target: complex) -> int:
-    queue = deque([(0, graph[start])])
+    queue: deque[Tuple[int, Node]] = deque([(0, graph[start])])
     visited = {start}
 
     while queue:
@@ -114,7 +127,72 @@ def bfs(graph: Dict[complex, Node], start: complex, target: complex) -> int:
 
 
 def A_star_search(graph: Dict[complex, Node], start: complex, target: complex) -> int:
-    ...
+    manhattan = lambda pos: abs(pos.real - target.real) + abs(pos.imag - target.imag)
+    search_details = {start: ASearch(h := manhattan(start), 0, h)}
+
+    open_list: List[Tuple[float, int, Node]] = [(h, 0, graph[start])]  # (f, g, node)
+    closed_list = {key: False for key in graph}
+
+    while open_list:
+        _, nb_steps, curr = heapq.heappop(open_list)
+
+        if curr.pos == target:
+            return nb_steps
+
+        closed_list[curr.pos] = True
+
+        for adj in curr.adj:
+            successor = graph[adj]
+
+            if closed_list[successor.pos]:  # Closed already
+                continue
+
+            g = nb_steps + 1
+            h = manhattan(successor.pos)
+            f = g + h
+
+            if (successor.pos not in search_details) or (
+                search_details[successor.pos].f > f
+            ):
+                heapq.heappush(open_list, (f, g, successor))
+                search_details[successor.pos] = ASearch(f, g, h)
+
+    return -1
+
+
+def dijkstra(graph: Dict[complex, Node], start: complex, target: complex) -> int:
+    distances = {}
+    distances[start] = 0
+    visited = []
+    min_curr_dist = float("inf")
+
+    heap: List[Tuple[int, Node]] = [(0, graph[start])]
+    while heap:
+        nb_steps, curr = heapq.heappop(heap)
+
+        if curr.pos == target:
+            min_curr_dist = nb_steps
+
+        visited.append(curr.pos)
+
+        if (
+            nb_steps >= min_curr_dist
+        ):  # Nb of steps only increase so no need to keep developing this path
+            continue
+
+        for adj in curr.adj:
+            successor = graph[adj]
+
+            if successor.pos in visited:  # Visited already
+                continue
+
+            if (successor.pos not in distances) or (
+                distances[successor.pos] > nb_steps + 1
+            ):
+                heapq.heappush(heap, (nb_steps + 1, successor))
+                distances[successor.pos] = nb_steps + 1
+
+    return distances.get(target, -1)
 
 
 @Timer.timeit
@@ -122,6 +200,8 @@ def find_best_path_from_start(
     graph: Dict[complex, Node], target: complex, start: complex
 ) -> int:
     return bfs(graph, start, target)
+    # return A_star_search(graph, start, target)
+    # return dijkstra(graph, start, target)
 
 
 @Timer.timeit
@@ -133,12 +213,14 @@ def find_best_start(
             nb_steps
             for candidate in candidates
             if (nb_steps := bfs(graph, candidate, target)) > 0
+            # if (nb_steps := A_star_search(graph, candidate, target)) > 0
+            # if (nb_steps := dijkstra(graph, candidate, target)) > 0
         )
     )
 
 
 @Timer.timeit
-def parse(filename: str) -> List[Tuple[str, int]]:
+def parse(filename: str) -> List[str]:
     with open(filename, "r") as file:
         heightmap = file.read().strip().split("\n")
     return heightmap
@@ -147,9 +229,9 @@ def parse(filename: str) -> List[Tuple[str, int]]:
 @Timer.timeit
 def solve(filename: str) -> Tuple[int, int]:
     heightmap = parse(filename)
-    graph, target, start, candidates = build_graph(heightmap)
-    part1 = find_best_path_from_start(graph, target, start)
-    part2 = find_best_start(graph, target, candidates)
+    graph, target, candidates = build_graph(heightmap)
+    part1 = find_best_path_from_start(graph, target, candidates[0])
+    part2 = find_best_start(graph, target, candidates[1:])
 
     return part1, part2
 
