d871ffd578f94e1a31f7f7b818f18d4f1cdae745
ianmnz <ianmemar@gmail.com>
2024-12-04 14:19:11 +0100

    AoC 2024 04

---
+++ src/y2024/d04.py
@@ -0,0 +1,99 @@
+# Advent of Code : Day 04 - Ceres Search
+# https://adventofcode.com/2024/day/04
+
+import os
+
+from helpers import Timer
+
+
+@Timer.timeit
+def add_padding(puzzle: list[str], pad: int, char: str = "*") -> list[str]:
+    n = len(puzzle[0])
+    pad_row: str = char * (n + 2 * pad)
+    pad_col: str = char * pad
+    return (
+        [pad_row] * pad + [pad_col + row + pad_col for row in puzzle] + [pad_row] * pad
+    )
+
+
+@Timer.timeit
+def searchXMAS(puzzle: list[str]) -> int:
+    pattern = "XMAS"
+    pad = len(pattern) - 1
+    padded = add_padding(puzzle, pad)
+
+    def substr(pos: tuple[int, int], dir: tuple[int, int]) -> str:
+        x, y = pos
+        dx, dy = dir
+        return "".join([padded[x + i * dx][y + i * dy] for i in range(len(pattern))])
+
+    count = 0
+    for i in range(pad, len(puzzle) + pad):
+        for j in range(pad, len(puzzle[0]) + pad):
+            if padded[i][j] != "X":
+                continue
+
+            pos = (i, j)
+
+            count += substr(pos, (0, +1)) == pattern  # horizontal right
+            count += substr(pos, (0, -1)) == pattern  # horizontal left
+
+            count += substr(pos, (+1, 0)) == pattern  # vertical down
+            count += substr(pos, (-1, 0)) == pattern  # vertical up
+
+            count += substr(pos, (+1, +1)) == pattern  # main diagonal down
+            count += substr(pos, (-1, -1)) == pattern  # main diagonal up
+
+            count += substr(pos, (-1, +1)) == pattern  # anti diagonal up
+            count += substr(pos, (+1, -1)) == pattern  # anti diagonal down
+
+    return count
+
+
+@Timer.timeit
+def searchX_MAS(puzzle: list[str]) -> int:
+    # Rotate patterns for the corners
+    patterns = ["MSSM", "SSMM", "SMMS", "MMSS"]
+    pad = 1
+    padded = add_padding(puzzle, pad)
+
+    count = 0
+    for i in range(pad, len(puzzle) + pad):
+        for j in range(pad, len(puzzle[0]) + pad):
+            if padded[i][j] != "A":
+                continue
+
+            corners = "".join(
+                [
+                    padded[i - 1][j - 1],  # top-left
+                    padded[i - 1][j + 1],  # top-right
+                    padded[i + 1][j + 1],  # bottom-right
+                    padded[i + 1][j - 1],  # bottom-left
+                ]
+            )
+
+            count += corners in patterns
+
+    return count
+
+
+@Timer.timeit
+def parse(filename: os.PathLike) -> list[str]:
+    with open(filename, "r") as file:
+        puzzle = file.read().splitlines()
+    return puzzle
+
+
+@Timer.timeit
+def solve(filename: os.PathLike) -> tuple[int, int]:
+    puzzle = parse(filename)
+    part1 = searchXMAS(puzzle)
+    part2 = searchX_MAS(puzzle)
+
+    return part1, part2
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    print(solve(Path(__file__).parents[2] / "data" / "2024" / "04.txt"))
--- tests/test_2024.py
+++ tests/test_2024.py
@@ -35,3 +35,12 @@ def test_day_03(data_dir):
 
     assert res1 == 183_788_984, f"Part1 = {res1}"
     assert res2 == 62_098_619, f"Part2 = {res2}"
+
+
+def test_day_04(data_dir):
+    from src.y2024.d04 import solve
+
+    res1, res2 = solve(data_dir / "04.txt")
+
+    assert res1 == 2_358, f"Part1 = {res1}"
+    assert res2 == 1_737, f"Part2 = {res2}"
